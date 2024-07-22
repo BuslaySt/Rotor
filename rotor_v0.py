08b5f872905ff08c6fa486a0b641a47b89af722f
Stepan Martyushov <techseth@yandex.ru>
2024-07-22 17:33:06 +0300

    Update rotor_v0.py

--- rotor_v0.py
+++ rotor_v0.py
@@ -6,17 +6,32 @@ class MainUI(QMainWindow):
     def __init__(self):
         super(MainUI, self).__init__()
         loadUi("rotor_ui.ui", self)
+        # Окно инициализации
         self.tab1_dateEdit.setDate(datetime.datetime.now())
         self.tab1_dateEdit.setCalendarPopup(True)
         self.tab1_timeEdit.setTime(datetime.datetime.now().time())
+        # Окно настроек
+        self.tab2_cBox_ScanStepGeneratrix.addItems(['0,5 мм', '1 мм', '2 мм'])
+        self.tab2_cBox_ScanStepGeneratrix.setCurrentIndex(1)
+        self.tab2_cBox_ScanStepGeneratrix.currentIndexChanged.connect(self.SetupScanStepGeneratrix)
+
+    def SetupScanStepGeneratrix(self, index: int) -> None:
+        match index:
+            case 0:
+                pass
+            case 1:
+                pass
+            case 2:
+                pass
 
 
    
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    # app = QApplication([])
-    harm = MainUI()
-    harm.show()
+    # app = QApplication(sys.argv)
+    app = QApplication([])
     
-    app.exec_()
-    # sys.exit(app.exec_())
+    rotor = MainUI()
+    rotor.show()
+    
+    # app.exec_()
+    sys.exit(app.exec_())
