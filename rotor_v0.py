7f89c0dabea8f5fb64613846fc885d28f23d12dd
Stepan Martyushov <techseth@yandex.ru>
2024-07-22 15:26:40 +0300

    Update rotor_v0.py

--- rotor_v0.py
+++ rotor_v0.py
@@ -1,11 +1,15 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.uic import loadUi
-import sys
+import sys, datetime
 
 class MainUI(QMainWindow):
     def __init__(self):
         super(MainUI, self).__init__()
         loadUi("rotor_ui.ui", self)
+        self.tab1_dateEdit.setDate(datetime.datetime.now())
+        self.tab1_dateEdit.setCalendarPopup(True)
+        self.tab1_timeEdit.setTime(datetime.datetime.now().time())
+
 
    
 if __name__ == '__main__':
